641abcdde8ce5f56424dc8520c1e7201bc938a3d
firedeathz <mikazzz_pro@hotmail.com>
2014-03-24 10:22:51 +0000

    Created the project skeleton

---
+++ PADI-DSTM/DATA-SERVER/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DATA_SERVER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ PADI-DSTM/PADI-CLIENT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PADI_CLIENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine("I'm now running. Your message: " + s);
+        }
+    }
+}
