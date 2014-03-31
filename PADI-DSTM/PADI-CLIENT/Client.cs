c45b6b800d21f79f6071407a3ba4882936a6fcc1
firedeathz <mikazzz_pro@hotmail.com>
2014-03-31 15:58:02 +0100

    Implemented Init and createPadInt on client side

--- PADI-DSTM/PADI-CLIENT/Client.cs
+++ PADI-DSTM/PADI-CLIENT/Client.cs
@@ -5,23 +5,98 @@ using System.Text;
 
 namespace PADI_DSTM
 {
-    class Client
+    public class PadiDstm : PadiLib
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("CLIENT APPLICATION v1.0");
+        IMasterServer masterServer;
 
+        public bool Init()
+        {
             Console.WriteLine("What is the master port?");
             string master_port = Console.ReadLine();
 
             Console.WriteLine("What is the master hostname?");
             string master_hostname = Console.ReadLine();
 
-            IMasterServer master = (IMasterServer)Activator.GetObject(
+            masterServer = (IMasterServer)Activator.GetObject(
                 typeof(IMasterServer),
                 "tcp://" + master_hostname + ":" + master_port + "/RemoteMasterServer");
 
-            master.Status();
+            return masterServer.Status();
+        }
+
+        public bool TxBegin()
+        {
+            return false;
+        }
+
+        public bool TxCommit()
+        {
+            return false;
+        }
+
+        public bool TxAbort()
+        {
+            return false;
+        }
+
+        public bool Status()
+        {
+            Console.WriteLine("Printing Status");
+            Console.WriteLine("---------------");
+            return true;
+        }
+
+        public bool Fail(string url)
+        {
+            return false;
+        }
+
+        public bool Freeze(string url)
+        {
+            return false;
+        }
+
+        public bool Recover(string url)
+        {
+            return false;
+        }
+
+        public PadInt CreatePadInt(int uid)
+        {
+            PadIntMetadata metadata = masterServer.CreatePadInt(uid);
+            
+            foreach (string s in metadata.servers)
+            {
+                IDataServer server = (IDataServer)Activator.GetObject(
+                    typeof(IDataServer),
+                    s);
+                server.CreatePadInt(uid);
+            }
+            return new PadInt(uid);
+        }
+
+        public PadInt AccessPadInt(int uid) 
+        {
+            //TODO: fix access
+            return new PadInt(uid);
+        }
+    }
+
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CLIENT APPLICATION v1.0");
+
+            PadiDstm dstm = new PadiDstm();
+
+            dstm.Init();
+
+            PadInt p = dstm.CreatePadInt(0);
+            p.Write(30);
+            
+            Console.WriteLine(p.Read());
+
             Console.ReadLine();
         }
     }
