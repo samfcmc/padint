468551d00bbfc2fe1fd2501b1f703fcf5e0431f3
firedeathz <mikazzz_pro@hotmail.com>
2014-03-31 11:49:38 +0100

    Added RemoteDataServer

--- PADI-DSTM/DATA-SERVER/DataServer.cs
+++ PADI-DSTM/DATA-SERVER/DataServer.cs
@@ -11,4 +11,59 @@ namespace PADI_DSTM
         {
         }
     }
+
+    public class RemoteDataServer : MarshalByRefObject, IServer
+    {
+        public bool Init()
+        {
+            return true;
+        }
+
+        public bool TxBegin()
+        {
+            return false;
+        }
+
+        public bool TxCommit()
+        {
+            return false;
+        }
+
+        public bool TxAbort()
+        {
+            return false;
+        }
+
+        public bool Status()
+        {
+            Console.WriteLine("Printing Status");
+            Console.WriteLine("---------------");
+            return true;
+        }
+
+        public bool Fail(string url)
+        {
+            return false;
+        }
+
+        public bool Freeze(string url)
+        {
+            return false;
+        }
+
+        public bool Recover(string url)
+        {
+            return false;
+        }
+
+        public PadInt CreatePadInt(int uid)
+        {
+            return new PadInt();
+        }
+
+        public PadInt AccessPadInt(int uid)
+        {
+            return null;
+        }
+    }
 }
