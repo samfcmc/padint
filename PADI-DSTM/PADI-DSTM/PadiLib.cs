24fb565d1414fbc426ea7d06a4ea2360e296eada
firedeathz <mikazzz_pro@hotmail.com>
2014-04-07 10:00:51 +0100

    Deleted PadiLib interface (useless)

--- PADI-DSTM/PADI-DSTM/PadiLib.cs
+++ PADI-DSTM/PADI-DSTM/PadiLib.cs
@@ -5,18 +5,107 @@ using System.Text;
 
 namespace PADI_DSTM
 {
-    public interface PadiLib
+    public class PadiDstm
     {
-        bool Init();
-        bool TxBegin();
-        bool TxCommit();
-        bool TxAbort();
-        bool Status();
-        bool Fail(string url);
-        bool Freeze(string url);
-        bool Recover(string url);
+        static IMasterServer masterServer;
+        static string masterPort;
+        static string masterHostname;
 
-        PadInt CreatePadInt(int uid);
-        PadInt AccessPadInt(int uid);
+        public static bool Init()
+        {
+            Console.WriteLine("What is the master port?");
+            masterPort = Console.ReadLine();
+
+            Console.WriteLine("What is the master hostname?");
+            masterHostname = Console.ReadLine();
+
+            masterServer = (IMasterServer)Activator.GetObject(
+                typeof(IMasterServer),
+                "tcp://" + masterHostname + ":" + masterPort + "/RemoteMasterServer");
+
+            return true;
+        }
+
+        public static bool TxBegin()
+        {
+            return false;
+        }
+
+        public static bool TxCommit()
+        {
+            return false;
+        }
+
+        public static bool TxAbort()
+        {
+            return false;
+        }
+
+        public static bool Status()
+        {
+            Console.WriteLine("Printing Status");
+            Console.WriteLine("---------------");
+            if (masterServer.Status())
+            {
+                Console.WriteLine("MasterServer is alive.");
+            }
+
+            foreach (KeyValuePair<string, IDataServer> server in masterServer.getDataServers())
+            {
+                if (server.Value.Status())
+                {
+                    Console.WriteLine("DataServer " + server.Key);
+                }
+            }
+            return true;
+        }
+
+        public static bool Fail(string url)
+        {
+            return false;
+        }
+
+        public static bool Freeze(string url)
+        {
+            return false;
+        }
+
+        public static bool Recover(string url)
+        {
+            return false;
+        }
+
+        public static PadInt CreatePadInt(int uid)
+        {
+            PadIntMetadata metadata = masterServer.CreatePadInt(uid);
+            if (metadata == null)
+            {
+                return null;
+            }
+
+            PadInt p = null;
+            foreach (string s in metadata.servers)
+            {
+                IDataServer server = (IDataServer)Activator.GetObject(
+                    typeof(IDataServer),
+                    s);
+                p = server.CreatePadInt(uid);
+            }
+            return p;
+        }
+
+        public static PadInt AccessPadInt(int uid)
+        {
+            PadIntMetadata metadata = masterServer.AccessPadInt(uid);
+
+            foreach (string s in metadata.servers)
+            {
+                IDataServer server = (IDataServer)Activator.GetObject(
+                    typeof(IDataServer),
+                    s);
+                return server.AccessPadInt(uid);
+            }
+            return null;
+        }
     }
 }
