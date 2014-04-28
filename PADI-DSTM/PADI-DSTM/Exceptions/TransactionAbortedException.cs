edfc55d07dc0a7fe2e8007b29755cfefdf157fd0
firedeathz <mikazzz_pro@hotmail.com>
2014-04-28 11:49:29 +0100

    TransactionAbortedException is remote now

--- PADI-DSTM/PADI-DSTM/Exceptions/TransactionAbortedException.cs
+++ PADI-DSTM/PADI-DSTM/Exceptions/TransactionAbortedException.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace PADI_DSTM.Exceptions
 {
@@ -14,6 +15,17 @@ namespace PADI_DSTM.Exceptions
         public TransactionAbortedException(long timestamp, string message): base(message)
         {
             this.timestamp = timestamp;
+        }
+
+        public TransactionAbortedException(SerializationInfo info, StreamingContext context)
+        {
+            timestamp = info.GetInt64("timestamp");
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("timestamp", timestamp);
         }
     }
 }
